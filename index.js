39fa96311b9dbd22f6d36cc270b98bf443ee74bf
Trong Linh <linhpksw@gmail.com>
2022-06-26 23:51:59 +0700

    add env PORT

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import usersRoutes from './routes/user.js';
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use('/users', usersRoutes);
