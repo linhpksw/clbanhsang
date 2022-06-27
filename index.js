969f66fee112caa558c96e6ab8a597a0ef8bdf64
Trong Linh <linhpksw@gmail.com>
2022-06-27 18:08:29 +0700

    test server

--- index.js
+++ index.js
@@ -10,4 +10,4 @@ app.use('/users', usersRoutes);
 app.listen(PORT, () =>
     console.log(`Server is running on PORT: http://localhost:${PORT}`)
 );
-app.get('/', (req, res) => res.send('Hello Linh.'));
+app.get('/', (req, res) => res.send('Hello Wordl.'));
