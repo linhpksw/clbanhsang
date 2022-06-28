cc83cd6018855de694e28eefdd8b7eecca477ab1
Trong Linh <linhpksw@gmail.com>
2022-06-28 16:47:22 +0700

    fix bugs

--- index.js
+++ index.js
@@ -11,4 +11,5 @@ app.use('/users', usersRoutes);
 app.listen(PORT, () =>
     console.log(`Server is running on PORT: http://localhost:${PORT}`)
 );
+
 app.get('/', (req, res) => res.send('Hello World.'));
