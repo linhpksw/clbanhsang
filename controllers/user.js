4c6317c094b7ae5553a5a2a65f00ebccbc23d577
Trong Linh <linhpksw@gmail.com>
2022-06-28 16:41:55 +0700

    update code

--- controllers/user.js
+++ controllers/user.js
@@ -4,22 +4,4 @@ let requests = [];
 export const userRequest = (req, res) => {
     const request = req.body;
     requests.push(request);
-    // users.push({ ...user, id: uuidv4() });
-    // res.send(`User with the name ${user.firstName} added to the database!`);
 };
-
-// export const getUsers = (req, res) => {
-//     res.send(users);
-// };
-
-// export const getUser = (req, res) => {
-//     const { id } = req.params;
-//     const foundUser = users.find((user) => user.id === id);
-//     res.send(foundUser);
-// };
-
-// export const deleteUser = (req, res) => {
-//     const { id } = req.params;
-//     users = users.filter((user) => user.id !== id);
-//     res.send(`User with the id ${id} deleted from the database.`);
-// };
--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,6 @@ import express from 'express';
 import { userRequest } from '../controllers/user.js';
 
 const router = express.Router();
-
-// router.get('/', getUsers);
 router.post('/', userRequest);
-// router.get('/:id', getUser);
-// router.delete('/:id', deleteUser);
 
 export default router;
