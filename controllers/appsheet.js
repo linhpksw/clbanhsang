7515fd2fc0cf3f9bbd66e3077c2a10ebee024555
Trong Linh <linhpksw@gmail.com>
2022-07-15 17:15:31 +0700

    L7

--- controllers/appsheet.js
+++ controllers/appsheet.js
@@ -183,7 +183,7 @@ export const deleteStudentRequest = async (req, res) => {
         await ZaloAPI.sendMessage(accessToken, '4966494673333610309', successContent);
 
         // Doi tag hoc sinh tu Dang hoc >>> Nghi hoc tren Zalo OA Chat
-        const isStudentIdExistInZaloColl = zaloColl.findOne({
+        const isStudentIdExistInZaloColl = await zaloColl.findOne({
             students: { zaloStudentId: studentId },
         });
         // const isStudentIdExistInZaloColl = zaloColl.find({
