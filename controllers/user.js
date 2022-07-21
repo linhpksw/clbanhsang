aadbc9e827eda0987ec379bc3f482df8ff841164
Trong Linh <linhpksw@gmail.com>
2022-07-21 13:34:05 +0700

    Finish

--- controllers/user.js
+++ controllers/user.js
@@ -636,7 +636,9 @@ export const updateRequest = async (req, res) => {
             }
         });
 
-        console.log(bulkWriteStudentInfo);
+        const result = await studentInfoColl.bulkWrite(bulkWriteStudentInfo);
+
+        console.log(result);
 
         res.send('Done');
     } catch (err) {
