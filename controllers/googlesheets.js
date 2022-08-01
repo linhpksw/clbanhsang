a8ac422bed497761f9077e29d6dff49140b988b2
Trong Linh <linhpksw@gmail.com>
2022-08-01 11:22:44 +0700

    Fix bug

--- controllers/googlesheets.js
+++ controllers/googlesheets.js
@@ -165,8 +165,7 @@ async function sendMessageBulk(client, sourceId, lastCol, lastRow, template) {
         // Loops through all the rows of data
         for (let i = 0; i < obj.length; i++) {
             const row = obj[i];
-            console.log(row);
-            const zaloUserId = row[1];
+            const zaloUserId = row['{ZID}'];
             const content = fillInTemplateFromObject(template, row);
 
             const result = await ZaloAPI.sendMessage(accessToken, zaloUserId, content);
