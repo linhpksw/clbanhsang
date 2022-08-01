77f7c84145652145ddf092ab909c354feb03741c
Trong Linh <linhpksw@gmail.com>
2022-08-02 01:16:07 +0700

    Update search from ClassId

--- routes/googlesheets.js
+++ routes/googlesheets.js
@@ -1,8 +1,15 @@
 import express from 'express';
-import { getListUser, sendListUser, getIncludeUser, searchNotRegister } from '../controllers/googlesheets.js';
+import {
+    getListUser,
+    sendListUser,
+    getIncludeUser,
+    searchNotRegister,
+    getListUserFromClassId,
+} from '../controllers/googlesheets.js';
 
 const router = express.Router();
 router.post('/search', getListUser);
+router.post('/searchClassId', getListUserFromClassId);
 router.post('/searchInclude', getIncludeUser);
 router.post('/searchNotRegister', searchNotRegister);
 router.post('/send', sendListUser);
