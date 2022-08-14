c650afbadc55af01d055365b39bb38b4b99e3fc2
Trong Linh <linhpksw@gmail.com>
2022-08-14 12:23:20 +0700

    Connect Casso with Zalo

--- controllers/casso.js
+++ controllers/casso.js
@@ -6,11 +6,35 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
+import * as Tools from './tool.js';
+import * as ZaloAPI from './zalo.js';
+import * as MongoDB from './mongo.js';
+
 export const cassoRequest = async (req, res) => {
     try {
-        const data = req.body;
-        console.log(data);
-        console.log('Kết nối thành công với Casso!');
+        await MongoDB.client.connect();
+        const db = MongoDB.client.db('zalo_servers');
+        const tokenColl = db.collection('tokens');
+        const { accessToken } = await MongoDB.readTokenFromDB(tokenColl);
+
+        const { data } = req.body;
+
+        for (let i = 0; i < data.length; i++) {
+            const { id, tid, description, amount, cusum_balance, when } = data[i];
+
+            const formatWhen = Tools.formatDateTime(when);
+            const formatAmount = `${amount > 0 ? 'tăng' : 'giảm'}  ${Tools.formatCurrency(amount)}`;
+            const formatCuSum = Tools.formatCurrency(cusum_balance);
+
+            const content = `Số dư tài khoản vừa ${formatAmount} vào ${formatWhen}
+Số dư hiện tại: ${formatCuSum}
+Nội dung: ${description}
+Mã giao dịch: ${id}
+Mã tham chiếu: ${tid}
+`;
+            await ZaloAPI.sendMessage(accessToken, '4966494673333610309', content);
+        }
+
         res.send('Done!');
     } catch (err) {
         console.error(err);
