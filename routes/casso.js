47a1325b63d169afb75ce1b7c55b67fd72b8e759
Trong Linh <linhpksw@gmail.com>
2022-09-07 23:24:08 +0700

    Open route for sync tuition

--- routes/casso.js
+++ routes/casso.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { cassoRequest, failExtract, unsendTransaction } from '../controllers/casso.js';
+import { cassoRequest, failExtract, unsendTransaction, syncTuition } from '../controllers/casso.js';
 
 const router = express.Router();
 router.post('/', cassoRequest);
 router.post('/failExtract', failExtract);
 router.post('/unsendTransaction', unsendTransaction);
+router.post('/syncTuition', syncTuition);
 
 export default router;
