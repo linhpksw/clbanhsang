76f1e4e3ca0b0e242c837d604331c55152001135
linhpksw <linhpksw@gmail.com>
2023-08-28 20:01:42 +0700

    fix block

--- controllers/gs.js
+++ controllers/gs.js
@@ -187,6 +187,10 @@ export const syncScore = async (req, res) => {
 
         const page = await browser.newPage();
 
+        await page.setUserAgent(
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        );
+
         try {
             await page.goto(LOGIN_URL);
             console.log('Navigated to login page');
